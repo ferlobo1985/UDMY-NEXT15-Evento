82759b418cd61e812e46b878e1005bca671bd811
Coding rev <fernando.lobo.prez@gmail.com>
2024-12-01 04:00:19 -0300

    11-Register form part 2
    11-Register form part 2

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,9 +3,10 @@ import { Input, Button } from "@nextui-org/react"
 import { useFormik } from "formik"
 import { useState } from "react"
 import * as Yup from "yup"
+import { errorHelper } from "@/components/utils"
 
 export default function RegisterPage(){
-    const [ formType, setFormType] = useState(false);
+    const [formType, setFormType] = useState(false);
 
     const formik = useFormik({
         initialValues:{ email:'',password:''},
@@ -23,10 +24,51 @@ export default function RegisterPage(){
     })
 
 
-
     return(
-        <form>
+        <form className="max-w-sm mx-auto" onSubmit={formik.handleSubmit}>
+            <h1 className="text-5xl py-10">
+                {formType ?"Register":"Sign in"}
+            </h1>
 
+            <div className="mb-5">
+                <Input
+                    type="email"
+                    label="Email"
+                    variant="bordered"
+                    fullWidth={true}
+                    {...formik.getFieldProps('email')}
+                    {...errorHelper(formik,'email')}
+                />
+            </div>
+
+            <div className="mb-5">
+                <Input
+                    type="password"
+                    label="Password"
+                    variant="bordered"
+                    fullWidth={true}
+                    {...formik.getFieldProps('password')}
+                    {...errorHelper(formik,'password')}
+                />
+            </div>
+
+
+
+            <div className="mb-3">
+                <Button color="secondary" type="submit">
+                    { formType ?"Register":"Sign in"}
+                </Button>
+            </div>
+            <div className="mb-3">
+                <Button color="primary" variant="bordered" 
+                onClick={()=>setFormType(!formType)}>
+                    { formType ?
+                        "Already registered ? Click here"
+                    :
+                        "Already signed in ? Click here"
+                    }
+                </Button>
+            </div>
 
         </form>
     )
