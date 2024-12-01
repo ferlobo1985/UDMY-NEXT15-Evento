70baa27bbe4a8834ae6a61f573edac092f1afed3
Coding rev <fernando.lobo.prez@gmail.com>
2024-12-01 04:38:07 -0300

    13-DB Config
    13-DB Config

---
+++ src/lib/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+if(!process.env.MONGODB_URI){
+    throw new Error("No MONGO DB URI env variable")
+}
+let cache = {connection:null,promise:null};
+
+export default async function DBconnect() {
+    if(cache.connection) return cache.connection;
+    if(!cache.promise){
+        cache.promise = mongoose.connect(process.env.MONGODB_URI)
+    }
+
+    try {
+        cache.connection = await cache.promise
+    } catch(error){
+        cache.promise = null;
+        throw error
+    }
+    return cache.connection
+}
