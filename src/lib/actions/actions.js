5176f331608bb51377baeb91d6f5b2e79b5f0986
Coding rev <fernando.lobo.prez@gmail.com>
2024-12-08 01:45:24 -0300

    21-Posting Venues
    21-Posting Venues

--- src/lib/actions/actions.js
+++ src/lib/actions/actions.js
@@ -1,9 +1,20 @@
 'use server'
+import DBconnect from '@/lib/db';
+import Venue from '@/lib/models/venue';
 
 export async function addVenue(prevState,formData){
+    await DBconnect()
     try{
+        // VALIDATION
 
+        const newVenue = new Venue({
+            name: formData.get('name'),
+            address: formData.get('address'),
+            state: formData.get('state')
+        });
+        await newVenue.save();
+        return { success: true, message:"Event added" }
     }catch(error){
-
+        return { success: false, message: [error.message] }
     }
 }
