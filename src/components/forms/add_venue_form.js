e4c3ece8442b3173c8a6575bce0f723cf9bc46d0
Coding rev <fernando.lobo.prez@gmail.com>
2024-12-08 01:31:26 -0300

    20-Add Venue Form
    20-Add Venue Form

--- src/components/forms/add_venue_form.js
+++ src/components/forms/add_venue_form.js
@@ -2,13 +2,53 @@
 
 import { Input, Button, Divider, Select, SelectItem } from "@nextui-org/react"
 import { states } from '@/components/states'
+import { addVenue } from "@/lib/actions/actions"
+import { useActionState } from "react"
 
 export default function AddVenueComponent(){
-
+    const [state,action,isPending] = useActionState(addVenue,null)
 
     return(
-        <>
-        FORM
-        </>
+      <form className="max-w-2xl mx-auto" action={action}>
+        <h1 className="text-2xl py-5">Add venue</h1>
+        <Divider className="mb-5"/>
+        
+        <Input
+            className="mb-5"
+            type="text"
+            label="Venue name"
+            variant="bordered"
+            fullWidth={true}
+            name="name"
+        />
+
+        <Input
+            className="mb-5"
+            type="text"
+            label="Address"
+            variant="bordered"
+            fullWidth={true}
+            name="address"
+        />
+
+        <Select
+            items={states}
+            label="State"
+            placeholder="Select the state"
+            fullWidth={true}
+            className="mb-5"
+            name="state"
+        >
+            {(state)=><SelectItem key={state.name}>{state.name}</SelectItem>}
+        </Select>
+
+        <Button color="secondary" variant="solid" type="submit">
+            Add Venue
+        </Button>
+
+
+
+
+      </form>
     )
 }
---
+++ src/lib/actions/actions.js
@@ -0,0 +1,9 @@
+'use server'
+
+export async function addVenue(prevState,formData){
+    try{
+
+    }catch(error){
+
+    }
+}
